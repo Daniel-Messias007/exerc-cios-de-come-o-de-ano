a923cf15cbf3b83f3b5ba8490311e66f8525e8ec
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2026-02-14 20:12:26 -0300

    Add files via upload

---
+++ exer 1/exer 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            y = 211;
+            x = 250;
+            
+            while (y <= x)
+            {
+                if (y % 2 == 0)
+                {
+                    Console.WriteLine("{0}", y);
+                }
+                y++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
