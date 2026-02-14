5202c0c6894d91a3229f48633ffd8a26064e7a58
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2026-02-14 20:13:22 -0300

    Add files via upload

---
+++ exer 3/exer 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[15];
+            int m;
+
+            Console.WriteLine("Digite 15 números inteiros para que o programa descubra o maior.");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("Digite o " + (i + 1) + "º número: ");
+                n[i] = int.Parse(Console.ReadLine());
+            }
+
+            m = n[0];
+
+            for (int i = 1; i < 15; i++)
+            {
+                if (n[i] > m)
+                {
+                    m = n[i];
+                }
+            }
+
+            Console.WriteLine("O seu maior número é: " + m);
+        }
+    }
+}
