828163573aa64b2a696daad349a43ee2ad41ccf1
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2026-02-14 20:12:51 -0300

    Add files via upload

---
+++ exer 2/exer 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f;
+
+            Console.WriteLine("Digite dois números.");
+            Console.WriteLine("Digite o primeiro número: ");
+            i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            f = int.Parse(Console.ReadLine());
+
+            while (f <= i)
+            {
+                if (f % 2 == 1)
+                {
+                    Console.WriteLine("{0}", f);
+                }
+                f++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
